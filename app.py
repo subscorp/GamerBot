86e03338a4dcaaa5d8a14c45bc8cfb68893e8cce
Ori <ori81510@gmail.com>
2021-04-18 17:38:35 +0300

    ...

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 import re
-import logging
 from flask import Flask, request
 import telegram
 from credentials import BOT_TOKEN, BOT_USERNAME, URL
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 
 global bot
@@ -12,21 +10,6 @@ TOKEN = BOT_TOKEN
 bot = telegram.Bot(token=TOKEN)
 
 app = Flask(__name__)
-
-
-def help(update, context):
-    """Send a message when the command /help is issued."""
-    update.message.reply_text('Help!')
-
-
-def echo(update, context):
-    """Echo the user message."""
-    update.message.reply_text(update.message.text)
-
-
-def error(update, context):
-    """Log Errors caused by Updates."""
-    logger.warning('Update "%s" caused error "%s"', update, context.error)
 
 
 @app.route('/{}'.format(TOKEN), methods=['POST'])
@@ -81,30 +64,3 @@ def hello():
 
 if __name__ == '__main__':
     app.run(threaded=True)
-    """Start the bot."""
-    # Create the Updater and pass it your bot's token.
-    # Make sure to set use_context=True to use the new context based callbacks
-    # Post version 12 this will no longer be necessary
-    updater = Updater(TOKEN, use_context=True)
-
-    # Get the dispatcher to register handlers
-    dp = updater.dispatcher
-
-    # on different commands - answer in Telegram
-    dp.add_handler(CommandHandler("help", help))
-
-    # on noncommand i.e message - echo the message on Telegram
-    dp.add_handler(MessageHandler(Filters.text, echo))
-
-    # log all errors
-    dp.add_error_handler(error)
-
-    # Start the Bot
-    updater.start_polling()
-
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
-    updater.idle()
-
-   
