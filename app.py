28811f1960d3b144c484d5403cc39e88e1b58d45
Ori <ori81510@gmail.com>
2021-04-20 22:27:20 +0300

    ..

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def respond():
         # send the welcoming message
         bot.sendMessage(chat_id=chat_id, text=bot_welcome, reply_to_message_id=msg_id)
     elif text == "tagOri":
-        message = "@Ori how are you?"
+        message = "@Ori  how are you?"
         bot.sendMessage(chat_id=chat_id, text=message)
 
     else:
