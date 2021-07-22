d38cee1bb0fec11f064a32cf8272ccbb7fd223cb
Ori <ori81510@gmail.com>
2021-07-22 22:37:14 +0300

    ...

--- app.py
+++ app.py
@@ -147,7 +147,7 @@ def send_message():
 @app.route('/get_free_games', methods=['GET', 'POST'])
 def notify_on_games():
     games = get_free_games()
-    bot.sendMessage(chat_id=-1001399023645, text='להלן משחקי האפיק החינמיים להשבוע:')
+    bot.sendMessage(chat_id=-1001399023645, text='להלן משחקי האפיק החינמיים לשבוע זה:')
     for game in games:
         bot.sendMessage(chat_id=-1001399023645, text=game)
     return "success sending free games"
